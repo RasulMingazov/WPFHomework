b8502eff456a91ea3b34f24ba6c2d735014d94ba
Bernad Jean <rasul.mingazov@gmail.com>
2022-03-20 02:05:58 +0300

    Calculator seems finished

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculator
+{
+    public partial class MainWindow : Window
+    {
+
+        private Stack<double> elements = new Stack<double>();
+        private Stack<char> operators = new Stack<char>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void AddDigitToValueClick(object sender, RoutedEventArgs e)
+        {
+            lblDisplay.Rules().CheckDigitAdding().ValidateSpecial((string) ((Button)e.OriginalSource).Content);
+        }
+
+        private void AddCommaToValueClick(object sender, RoutedEventArgs e)
+        {
+            lblDisplay.Rules().CheckCommaAdding().ValidateSpecial((string) ((Button)e.OriginalSource).Content);
+        }
+        private void CalculateClick(object sender, RoutedEventArgs e)
+        {
+            lblDisplay.Rules().CheckValue();
+            elements.Push(Convert.ToDouble((string) lblDisplay.Content));
+            operators.Push(((string) ((Button)e.OriginalSource).Content).ToCharArray()[0]);
+            lblDisplay.Content = "0";
+        }
+        private void ClearClick(object sender, RoutedEventArgs e)
+        {
+            lblDisplay.Content = "0";
+            elements.Clear();
+            operators.Clear();
+        }
+        private void EqualsClick(object sender, RoutedEventArgs e)
+        {
+            if (elements.Count > 0)
+            {
+                lblDisplay.Rules().CheckValue();
+                double FirstNumber = elements.Peek();
+                double SecondNumber = Convert.ToDouble((string)lblDisplay.Content);
+
+                string result = calculate(operators.Peek(), FirstNumber, SecondNumber).ToString();
+
+                lblDisplay.Rules(result).CheckResultValue().Validate();
+
+
+                lblDisplay.Content = calculate(operators.Peek(), FirstNumber, SecondNumber).ToString();
+
+                elements.Push(SecondNumber);
+            }
+
+        }
+        private void ChangeClick(object sender, RoutedEventArgs e)
+        {
+            if (lblDisplay.Content.ToString()!.StartsWith("-"))
+            {
+                lblDisplay.Content = lblDisplay.Content.ToString()!.Substring(1);
+            }
+            else
+            {
+                lblDisplay.Content = "-" + lblDisplay.Content.ToString();
+            }
+
+        }
+        private double calculate(char o, double a, double b)
+        {
+            switch (o)
+            {
+                case '+': 
+                    return a + b;
+                case '-':
+                    return a - b;
+                case '*':   
+                    return a * b;
+                case '/':   
+                    return a / b;
+            }
+            return 0;
+        }
+    }
+    static class ControlValidationExtension
+    {
+        public static Validator Rules(this Label control) => new Validator(control, (string)control.Content);
+        public static Validator Rules(this Label control, string value) => new Validator(control, value);
+    }
+}
---
+++ Calculator/Validator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Controls;
+
+
+namespace Calculator
+{
+    class Validator
+    {
+
+        private readonly Label _label;
+        private readonly string _content;
+
+        private bool _success = false;
+
+        public Validator(Label label, string content)
+            => (_label, _content) = (label, content);
+        public Validator CheckCommaAdding()
+        {
+            if (_content != null && _content != "" && !_content.Contains(','))
+                _success = true;
+            return this;
+        }
+        public Validator CheckDigitAdding()
+        {
+            if (_content == "0")
+                _label.Content = "";
+
+            ChangeFontSize();
+
+            if (string.Concat(_content.Where(char.IsDigit)).Length < 16)
+                _success = true;
+
+            return this;
+        }
+        public Validator CheckResultValue()
+        {
+            ChangeFontSize();
+
+            if (string.Concat(_content.Where(char.IsDigit)).Length < 16)
+                _success = true;
+            return this;
+        }
+        private void ChangeFontSize()
+        {
+            if (_content.Length >= 8)
+                _label.FontSize = 40;
+
+            if (_content.Length >= 12)
+                _label.FontSize = 32;
+        }
+        public void CheckValue()
+        {
+            if (_content.EndsWith(','))
+                _label.Content += "0";           
+        }
+        public void Validate()
+        {
+            if (_success) 
+                _label.Content = _content;
+        }
+        public void ValidateSpecial(String AddingElement)
+        {
+            if (_success)
+                _label.Content +=  AddingElement;
+            
+        }
+    }
+}
