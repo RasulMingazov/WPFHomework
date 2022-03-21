81e5484c0dcaa2d140344ccfe31609c83ba6fc14
Bernad Jean <rasul.mingazov@gmail.com>
2022-03-21 11:36:31 +0300

    Project created and example solution

---
+++ TestTask/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows;
+
+
+namespace TestTask
+{
+    /// &lt;summary&gt;
+    /// Interaction logic for MainWindow.xaml
+    /// &lt;/summary&gt;
+    public partial class MainWindow : Window
+    {
+        Test[] myTest = new Test[10];
+
+        int i, k;
+        public static int pv;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (myTest[i - 1].PV == pv)
+                k++;
+            /* if (i-1 == 0 &amp;&amp; Rb2.IsChecked == true)
+            k++;
+            if (i-1 == 1 &amp;&amp; Rb1.IsChecked == true)
+            k++;
+            if (i-1 == 2 &amp;&amp; Rb1.IsChecked == true)
+            k++;*/
+
+            MethodNext(i);
+            if (i >= 4)
+{
+                btnNext.IsEnabled = false;
+                MessageBox.Show(k.ToString());
+            }
+            i++;
+        }
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            pv = 0;
+            for (i = 0; i < 10; i++)
+{
+                myTest[i] = new Test();
+            }
+            myTest[0].Q = "2+2=?"; ;
+            myTest[0].V1 = "5"; ;
+            myTest[0].V2 = "4";
+            myTest[0].V3 = "6";
+            myTest[0].PV = 2;
+            myTest[1].Q = "9 + 3 =?";
+            myTest[1].V1 = "12";
+            myTest[1].V2 = "14";
+            myTest[1].V3 = "16";
+            myTest[1].PV = 1;
+            myTest[2].Q = "3 + 5 =?";
+            myTest[2].V1 = "6";
+            myTest[2].V2 = "9";
+            myTest[2].V3 = "8";
+            myTest[2].PV = 3;
+            myTest[3] = new Test("5 + 16 =?", "25", "31","32", 2);
+            i = 1;
+            k = 0;
+            MethodNext(0);
+        }
+        private void Rb1_Checked(object sender, RoutedEventArgs e)
+        {
+            pv = 1;
+        }
+        private void Rb2_Checked(object sender, RoutedEventArgs e)
+        {
+            pv = 2;
+        }
+        private void Rb3_Checked(object sender, RoutedEventArgs e)
+        {
+            pv = 3;
+        }
+        private void MethodNext(int i)
+        {
+            lbl1.Content = myTest[i].Q;
+            Rb1.Content = myTest[i].V1;
+            Rb2.Content = myTest[i].V2;
+            Rb3.Content = myTest[i].V3;
+        }
+    }
+}
---
+++ TestTask/Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TestTask
+{
+    class Test
+    {
+        public String Q;
+        public String V1;
+        public String V2;
+        public String V3; public int PV;
+
+        public Test()
+        {
+            Q = "";
+            V1 = "";
+            V2 = "";
+            V3 = "";
+            PV = 0;
+        }
+
+        public Test(string q, string v1, string v2, string v3, int pv)
+        {
+            Q = q;
+            V1 = v1;
+            V2 = v2;
+            V3 = v3;
+            PV = pv;
+        }
+    }
+}
